03f8e80b969fa08fc3d3b7d0539c85c447073bb2
phoenixx-png <jojoeydenver@gmail.com>
2025-11-04 11:11:13 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,16 @@
+
+let name = "Robbie"
+
+let greet = function(name){
+    return "Hello, " + name + "!"
+}
+
+let add = function(a, b){
+    return a + b
+}
+
+let greetMessage = greet(name)
+let sum = add(7, 992)
+
+console.log(greetMessage);
+console.log("The sum is: " + sum);
